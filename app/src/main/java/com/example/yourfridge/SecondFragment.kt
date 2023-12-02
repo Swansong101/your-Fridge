be7383ff61fbf739e03a46677eeacef11e2479da
Ian-Mutuge <99174650+Ian-Mutuge@users.noreply.github.com>
2023-12-02 15:45:10 +0300

    Some design

--- app/src/main/java/com/example/yourfridge/SecondFragment.kt
+++ app/src/main/java/com/example/yourfridge/SecondFragment.kt
@@ -10,8 +10,10 @@ import androidx.navigation.fragment.findNavController
 class SecondFragment : Fragment() {
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
