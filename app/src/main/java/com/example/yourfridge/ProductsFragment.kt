9e1c1c65555a30c76c356547e9f76a3a0aa4bc25
Ian-Mutuge <99174650+Ian-Mutuge@users.noreply.github.com>
2023-12-03 15:00:20 +0300

    Camera images

--- app/src/main/java/com/example/yourfridge/ProductsFragment.kt
+++ app/src/main/java/com/example/yourfridge/ProductsFragment.kt
@@ -1,30 +1,83 @@
 package com.example.yourfridge
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.yourfridge.databinding.FragmentProductsBinding
 
-class
+class ProductsFragment : Fragment() {
 
-ProductsFragment : Fragment() {
+    private var _binding: FragmentProductsBinding? = null
+    private val binding get() = _binding!!
+
+    private val REQUEST_PERMISSION_CODE = 123
+    private val REQUEST_IMAGE_CAPTURE = 456 // Corrected to Int type
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_products, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<View>(R.id.button2).setOnClickListener {
+        binding.button2.setOnClickListener {
             findNavController().navigate(R.id.action_productsFragment_to_addedProducts)
         }
 
+        binding.imageView.setOnClickListener {
+            if (hasCameraPermission()) {
+                dispatchTakePictureIntent()
+            } else {
+                requestCameraPermission()
+            }
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    private fun handleImageCaptureResult(data: Intent?) {
+        val imageBitmap = data?.extras?.get("data") as? Bitmap
+        imageBitmap?.let {
+            // Do something with the captured image, for example, set it to the ImageView
+            binding.imageView.setImageBitmap(it)
+        }
+    }
+
+    private fun hasCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.CAMERA),
+            REQUEST_PERMISSION_CODE
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
